25337859fdd71c0529e798d22af34d961d40d044
Renaldi Arlin <renaldi.arlin@dansmultipro.com>
2023-10-11 17:48:05 +0700

    Initial Commit

---
+++ Nutreazy/NutreazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutreazyApp.swift
+//  Nutreazy
+//
+//  Created by Renaldi Arlin on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NutreazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
