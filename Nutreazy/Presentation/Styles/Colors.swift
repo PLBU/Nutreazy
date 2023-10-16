dad072321e0cd91a26ae5b43fb25fcebe2ec528a
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-10-16 16:13:43 +0700

    Add colors

---
+++ Nutreazy/Presentation/Styles/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  Nutreazy
+//
+//  Created by Renaldi Arlin on 16/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+// Colors
+let PRIMARY_COLOR = Color("PrimaryColor")
+let SECONDARY_COLOR = Color("SecondaryColor")
+let ACCENT_COLOR = Color("AccentColor")
+let BACKGROUND_COLOR = Color("BackgroundColor")
+let TEXT_COLOR = Color("SecondaryColor")
