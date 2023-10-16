77f2d3062b493fb794c2b8d65039bac4a9d22a41
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-10-16 16:24:04 +0700

    Add Text Styles

---
+++ Nutreazy/Presentation/Styles/TextStyles.swift
@@ -0,0 +1,20 @@
+//
+//  TextStyles.swift
+//  Nutreazy
+//
+//  Created by Renaldi Arlin on 16/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+let HEADING_1 = Font.custom("Poppins-Medium", size: 48)
+let HEADING_2 = Font.custom("Poppins-Medium", size: 40)
+let HEADING_3 = Font.custom("Poppins-Medium", size: 36)
+let HEADING_4 = Font.custom("Poppins-Medium", size: 32)
+let HEADING_5 = Font.custom("Poppins-Medium", size: 24)
+let PARARAGPH_1 = Font.custom("Poppins-Light", size: 18)
+let PARARAGPH_2 = Font.custom("Poppins-Light", size: 14)
+let CHIP = Font.custom("Poppins-Light", size: 14)
+let SUBHEADING_4 = Font.custom("Poppins-Light", size: 32)
+let SUBHEADING_5 = Font.custom("Poppins-Light", size: 24)
