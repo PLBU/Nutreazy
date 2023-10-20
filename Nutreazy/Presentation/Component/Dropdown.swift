864da71a569e4480e4743769a2625acc652f82e4
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-10-20 21:42:29 +0700

    Add showing Menu

--- Nutreazy/Presentation/Component/Dropdown.swift
+++ Nutreazy/Presentation/Component/Dropdown.swift
@@ -9,26 +9,33 @@ import SwiftUI
 
 struct Dropdown: View {
     @Binding private var value: String
+    @FocusState private var isFocused: Bool
     private let placeholder = "Pilih"
+    private var options: [String] = []
     private var unitName = ""
     private var width = Double.infinity
     
-    init(value: Binding<String>, label: String = "", width: Double = Double.infinity) {
+    init(value: Binding<String>, label: String = "", width: Double = Double.infinity, options: [String] = []) {
         self._value = value
         self.width = width
+        self.options = options
     }
+    
+    let colors = ["Red", "Green", "Blue"]
     
     
     var body: some View {
         HStack {
-            if value.isEmpty {
-                Text(placeholder)
+            Menu {
+                ForEach(options, id: \.self) {
+                    option in Text(option)
+                        .font(PARAGRAPH_1)
+                        .foregroundColor(TEXT_COLOR)
+                }
+            } label: {
+                Text(value.isEmpty ? placeholder : value)
                     .font(PARAGRAPH_1)
-                    .foregroundColor(ACCENT_COLOR)
-            } else {
-                Text(value)
-                    .font(PARAGRAPH_1)
-                    .foregroundColor(TEXT_COLOR)
+                    .foregroundColor(value.isEmpty ? ACCENT_COLOR : TEXT_COLOR)
             }
             Image(uiImage:
                 UIImage(named: "ic_dropdown")!.withConfiguration(
@@ -45,13 +52,6 @@ struct Dropdown: View {
             RoundedRectangle(cornerRadius: 16)
                 .stroke(PRIMARY_COLOR, lineWidth: 1)
         )
-        .onTapGesture {
-            print("I am tapped")
-        }
-    }
-    
-    public func getValue() -> String {
-        return value
     }
 }
 
@@ -59,7 +59,7 @@ struct Dropdown_Previews: PreviewProvider {
     
     static var previews: some View {
         VStack {
-            Dropdown(value: .constant(""))
+            Dropdown(value: .constant(""), options: ["Option 1", "Option 2", "Option 3"])
         }
     }
 }
