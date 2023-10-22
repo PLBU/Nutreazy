ee41ffdac0fe91786f14e99fe8dad094bd67e36e
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-10-22 21:08:35 +0700

    Add Constant Util

---
+++ Nutreazy/Util/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Nutreazy
+//
+//  Created by Renaldi Arlin on 22/10/23.
+//
+
+import Foundation
+
+let genderListID = ["Pria", "Wanita"]
+let activityIntensityListID = ["Mageran", "Ringan", "Sedang", "Tinggi", "Atlet"]
