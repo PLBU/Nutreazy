d4129b30e44629fcbd1b1472447d04fd7e2ec402
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-11-18 21:18:29 +0700

    Change Domain structure & add UserManager

---
+++ Nutreazy/Domain/User/UserManager.swift
@@ -0,0 +1,15 @@
+//
+//  UserManager.swift
+//  Nutreazy
+//
+//  Created by Renaldi Arlin on 18/11/23.
+//
+
+import Foundation
+import RealmSwift
+
+class UserManager {
+    static let instance = UserManager()
+    
+    private init() {}
+}
