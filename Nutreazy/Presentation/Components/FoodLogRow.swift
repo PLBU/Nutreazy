1da300beb75ef7cbca1e7098a2926b5f71b3b20d
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-11-25 11:23:30 +0700

    Add FoodLogRow component

---
+++ Nutreazy/Presentation/Components/FoodLogRow.swift
@@ -0,0 +1,50 @@
+//
+//  FoodLogRow.swift
+//  Nutreazy
+//
+//  Created by Renaldi Arlin on 25/11/23.
+//
+
+import SwiftUI
+
+struct FoodLogRow: View {
+    var foodLog: FoodLogModel
+    
+    var body: some View {
+        HStack{
+            Group {
+                Text(foodLog.foodInfo?.name ?? "") +
+                Text(" (" + String(foodLog.amount)) +
+                Text(String(foodLog.foodInfo?.servingName ?? "") + ")")
+            }
+            .font(PARAGRAPH_1)
+            .foregroundColor(TEXT_COLOR)
+            Spacer()
+            Chip(text: String(foodLog.calories) + "cal")
+        }
+    }
+}
+
+struct FoodLogRow_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodLogRow(
+            foodLog: FoodLogModel(
+                foodInfo: FoodInfoModel(
+                    name: "Boiled Chicken Breast",
+                    servingName: "g",
+                    servingSize: 1,
+                    calories: 22,
+                    carbohydrate: 1,
+                    protein: 2,
+                    fat: 1
+                ),
+                amount: 125,
+                mealType: MealType.Breakfast,
+                calories: 2750,
+                carbohydrate: 125,
+                protein: 250,
+                fat: 125
+            )
+        )
+    }
+}
